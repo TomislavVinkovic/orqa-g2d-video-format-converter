81cac67ff620c502e87d20e93cf8c32bd6bf68bd
tomislav <tomislav@archlinux.com>
2025-02-20 09:29:38 +0000

    RGBX support added

--- G2dPixelFormatConverter.cpp
+++ G2dPixelFormatConverter.cpp
@@ -125,17 +125,42 @@ int G2dPixelFormatConverter::getFormatSurface(
             surface.bottom = height / 2;
             surface.stride = width * 2; // 2 bytes per pixel
             break;
-        case G2D_RGB888:
+        case G2D_RGB888: // +
             surface.planes[0] = buf->buf_paddr;
             surface.bottom = height;
             surface.stride = width;
             break;
-        case G2D_RGBA8888:
+        case G2D_RGBA8888: // +
             surface.planes[0] = buf->buf_paddr;
             surface.bottom = height;
             surface.stride = width;
             break;
-        case G2D_RGB565:
+        case G2D_RGBX8888:
+            surface.planes[0] = buf->buf_paddr;
+            surface.bottom = height;
+            surface.stride = width * 2;
+            break;
+        case G2D_ARGB8888:
+            surface.planes[0] = buf->buf_paddr;
+            surface.bottom = height;
+            surface.stride = width;
+            break;
+        case G2D_XRGB8888:
+            surface.planes[0] = buf->buf_paddr;
+            surface.bottom = height;
+            surface.stride = width;
+            break;
+        case G2D_RGBA5551:
+            surface.planes[0] = buf->buf_paddr;
+            surface.bottom = height;
+            surface.stride = width;
+            break;
+        case G2D_RGBX5551:
+            surface.planes[0] = buf->buf_paddr;
+            surface.bottom = height;
+            surface.stride = width;
+            break;
+        case G2D_RGB565: // +
             surface.planes[0] = buf->buf_paddr;
             surface.bottom = height;
             surface.stride = width;
